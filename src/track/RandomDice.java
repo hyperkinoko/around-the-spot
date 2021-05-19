68970fc47006a55b87748a9d87e8bc5dbc279d72
松本キノコ <kinoko@matsumotokinokonoMacBook-Air.local>
2021-05-19 09:59:54 +0900

    はじめの状態

--- src/track/RandomDice.java
+++ src/track/RandomDice.java
@@ -1,14 +1,9 @@
 package track;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class RandomDice implements Dice {
     public ArrayList<Integer> roll(int n) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            list.add(new Random().nextInt(6) + 1);
-        }
-        return list;
+        return null;
     }
 }
