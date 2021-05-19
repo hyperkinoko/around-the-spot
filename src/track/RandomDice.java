13100082266d69377030540d9ec75c0037be7848
松本キノコ <kinoko@matsumotokinokonoMacBook-Air.local>
2021-05-19 11:58:54 +0900

    RandomDiceを実装

--- src/track/RandomDice.java
+++ src/track/RandomDice.java
@@ -1,9 +1,28 @@
 package track;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RandomDice implements Dice {
-    public ArrayList<Integer> roll(int n) {
-        return null;
+  /**
+    * 引数で指定された数のサイコロを振って、そのリストを返す
+    * @param n 一度に振るサイコロの数
+    * @return サイコロの目のリスト
+    */
+  @Override
+  public ArrayList<Integer> roll(int n) {
+    ArrayList<Integer> list = new ArrayList<>();
+    Random rand = new Random();
+    for(int i = 0; i < n; i++){
+      int me = rand.nextInt(6) + 1;
+      list.add(me);
     }
+    return list;
+  }
+
+  public static void main(String[] args) {
+    RandomDice randomDice = new RandomDice();
+    ArrayList<Integer> list = randomDice.roll(3);
+    System.out.println(list);
+  }
 }
